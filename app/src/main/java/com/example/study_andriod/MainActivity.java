344ccd90bf2541e3c6170f85e66a1da82cbcb650
김정현 <rlawjdgus843@gmail.com>
2021-03-14 18:08:33 +0900

    1. SharedPreferences(임시저장) - 설정 on/off 버튼에 주로 사용됨. 단, 앱 삭제시 사라짐

--- app/src/main/java/com/example/study_andriod/MainActivity.java
+++ app/src/main/java/com/example/study_andriod/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.study_andriod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private String str;
     ImageView img_test;
     private ListView list;
+    EditText et_save;
+    String shared = "file";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         btn_move = findViewById(R.id.btn_move);
         et_test = findViewById(R.id.et_test);
         list = (ListView)findViewById(R.id.list);
+        et_save = (EditText)findViewById(R.id.et_save);
 
         // Button
         btn_test.setOnClickListener(new View.OnClickListener() {
@@ -75,5 +79,24 @@ public class MainActivity extends AppCompatActivity {
         data.add("맥북");
         data.add("사자");
         adapter.notifyDataSetChanged(); // 저장
+
+        // SharedPreference
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        String value = sharedPreferences.getString("Kim", "");
+        et_save.setText(value);
+    }
+
+    // Activity를 벗어났을 때(뒤로 가기나 앱 종료 시 실행)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        
+        // (EditText)et_save에 입력한 값
+        String value = et_save.getText().toString();
+        editor.putString("Kim", value);
+        editor.commit();
     }
 }
